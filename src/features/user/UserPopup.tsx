853f00d98fc235db4b40c1968d9bee0c436e432e
nemanjamalesija <malesija13@gmail.com>
2023-09-21 09:45:42 +0200

    Improve animation on user popup

--- src/features/user/UserPopup.tsx
+++ src/features/user/UserPopup.tsx
@@ -4,77 +4,70 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser, faRightFromBracket } from '@fortawesome/free-solid-svg-icons';
 import { useState } from 'react';
 import { useNavigate } from 'react-router';
+import { useAppDispatch } from '../../hooks/useAppDispatch';
+import { setUser } from './userSlice';
 
-const ins = 'opacity-1 py-3';
-const outs = 'opacity-0 invisible';
+const ins = 'opacity-1  max-h-[300px] p-4';
+const outs = 'opacity-0 invisible max-h-0 ';
 
 const UserPopup = () => {
-  const [account, toggleAccount] = useState<boolean>(false);
   const navigate = useNavigate();
   const { full_name, picture } = useGetUserData();
+  const [userPopupOpen, setIsUserPopupOpen] = useState<boolean>(false);
+  const dispatch = useAppDispatch();
 
   const logOutFront = () => {
     localStorage.removeItem('jwt');
     navigate('/login');
+    dispatch(setUser({ username: '', email: '', full_name: '', picture: '' }));
   };
 
-  return (
-    <div className='smb:absolute -top-1 right-4 z-[99]'>
-      <div className='relative smb:pt-5 sm:pt-5 flex items-center justify-end max-h-16 gap-4 '>
-        <button onClick={() => toggleAccount(!account)}>
-          <UserImage
-            src={picture}
-            alt={full_name}
-            imgClassname='smb:h-9 smb:w-9 lg:h-10 lg:w-10 shadow-lg'
-          />
-        </button>
-        <div
-          className={`absolute bg-red top-1/2 -right-[10%] opacity-1 translate-y-[32.9%] -translate-x-[30%] bg-figmaGray rounded-lg text-figmaBlack shadow-lg transition-all duration-150  origin-top-right  ${
-            account ? ins : outs
-          }`}
-          style={account ? {} : { transform: 'scale(0.95)' }}
-        >
-          <ul className='flex flex-col gap-3 px-2 '>
-            {/* dummy list element */}
-            <li
-              className={`flex items-center justify-start gap-4 pl-2 pr-12 py-[3px] hover:bg-figmaGrayLight rounded-md cursor-pointer transition-colors duration-200 ${
-                ins ? 'flex' : 'hidden'
-              }`}
-              role='button'
-            >
-              <a
-                href='#'
-                className='bg-white h-8 w-8 flex items-center justify-center rounded-full cursor-pointer'
-              >
-                <FontAwesomeIcon
-                  className='text-base h-5 w-5
-              text-figmaBlue
-                hover:text-figmaBlueShade'
-                  icon={faUser}
-                />
-              </a>
-              <span>Account</span>
-            </li>
+  const accountBase =
+    'absolute top-[110%] right-[0.3%] bg-figmaGray flex flex-col text-base gap-2 rounded-md w-max max-h-0 shadow-lg transition-all duration-150 ease-in-out';
 
-            {/* log out */}
-            <li
-              className={`flex items-center justify-start gap-4 pl-2 pr-12 py-[3px] hover:bg-figmaGrayLight rounded-md cursor-pointer transition-colors duration-200 ${
-                ins ? 'flex' : 'hidden'
-              }`}
-              onClick={logOutFront}
-              role='button'
-            >
-              <p className='bg-white h-8 w-8 flex items-center justify-center rounded-full cursor-pointer'>
+  const opened = 'p-[0.8rem] max-h-[300px] visible opacity-1';
+  const closed = 'invisible opacity-0';
+
+  return (
+    <div className='absolute right-[5%] top-5 flex flex-col items-center gap-3 font-semibold rounded-lg'>
+      <div className='flex gap-3 items-center'>
+        <button onClick={() => setIsUserPopupOpen(!userPopupOpen)}>
+          <UserImage src={picture} alt={full_name} />
+        </button>
+
+        <div
+          className={`${
+            userPopupOpen
+              ? `${accountBase} ${opened}`
+              : `${accountBase} ${closed}`
+          }`}
+        >
+          <a
+            href='#'
+            className='p-[0.4rem] pr-[4rem] rounded-lg hover:bg-figmaGrayShade transition-all duration-200 inline-block'
+          >
+            <div className='flex items-center gap-2'>
+              <p className='bg-white rounded-full h-9 w-9 flex items-center justify-center'>
+                <FontAwesomeIcon icon={faUser} className='text-figmaBlue' />
+              </p>
+              <p className='text-figmaBlack'>My profile</p>
+            </div>
+          </a>
+          <a
+            href='/login'
+            className='p-[0.4rem] pr-[4rem] rounded-lg hover:bg-figmaGrayShade transition-all duration-200 inline-block'
+            onClick={logOutFront}
+          >
+            <div className='flex items-center gap-2'>
+              <p className='bg-white rounded-full h-9 w-9 flex items-center justify-center'>
                 <FontAwesomeIcon
-                  className='text-base h-5 w-5
-              text-figmaBlue
-              hover:text-figmaBlueShade'
                   icon={faRightFromBracket}
+                  className='text-figmaBlue'
                 />
               </p>
-              <span>Log out</span>
-            </li>
-          </ul>
+              <p className='text-figmaBlack'>Log out</p>
+            </div>
+          </a>
         </div>
       </div>
     </div>
