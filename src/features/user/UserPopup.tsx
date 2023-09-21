f938075912b03e5824d90a20600b6aa577b5a3ce
nemanjamalesija <malesija13@gmail.com>
2023-09-21 10:53:18 +0200

    Clean popup animation

--- src/features/user/UserPopup.tsx
+++ src/features/user/UserPopup.tsx
@@ -23,52 +23,51 @@ const UserPopup = () => {
   };
 
   const accountBase =
-    'absolute top-[110%] right-[0.3%] bg-figmaGray flex flex-col text-base gap-2 rounded-md w-max max-h-0 shadow-lg transition-all duration-150 ease-in-out';
+    'absolute top-[118%] right-[16%] bg-figmaGray flex flex-col text-base gap-2 rounded-md w-max max-h-0 shadow-lg transition-all duration-150 ease-in-out';
 
   const opened = 'p-[0.8rem] max-h-[300px] visible opacity-1';
   const closed = 'invisible opacity-0';
 
   return (
-    <div className='absolute right-[5%] top-5 flex flex-col items-center gap-3 font-semibold rounded-lg'>
-      <div className='flex gap-3 items-center'>
-        <button onClick={() => setIsUserPopupOpen(!userPopupOpen)}>
-          <UserImage src={picture} alt={full_name} />
-        </button>
+    <div className='absolute right-[5%] top-5 flex flex-col items-center gap-3 font-semibold rounded-lg '>
+      <button onClick={() => setIsUserPopupOpen(!userPopupOpen)}>
+        <UserImage src={picture} alt={full_name} />
+      </button>
 
-        <div
-          className={`${
-            userPopupOpen
-              ? `${accountBase} ${opened}`
-              : `${accountBase} ${closed}`
-          }`}
+      <div
+        className={`${
+          userPopupOpen
+            ? `${accountBase} ${opened}`
+            : `${accountBase} ${closed}`
+        }`}
+      >
+        <a
+          href='#'
+          className='p-[0.4rem] pr-[4rem] rounded-lg hover:bg-figmaGrayShade transition-all duration-200 inline-block'
         >
-          <a
-            href='#'
-            className='p-[0.4rem] pr-[4rem] rounded-lg hover:bg-figmaGrayShade transition-all duration-200 inline-block'
-          >
-            <div className='flex items-center gap-2'>
-              <p className='bg-white rounded-full h-9 w-9 flex items-center justify-center'>
-                <FontAwesomeIcon icon={faUser} className='text-figmaBlue' />
-              </p>
-              <p className='text-figmaBlack'>My profile</p>
-            </div>
-          </a>
-          <a
-            href='/login'
-            className='p-[0.4rem] pr-[4rem] rounded-lg hover:bg-figmaGrayShade transition-all duration-200 inline-block'
-            onClick={logOutFront}
-          >
-            <div className='flex items-center gap-2'>
-              <p className='bg-white rounded-full h-9 w-9 flex items-center justify-center'>
-                <FontAwesomeIcon
-                  icon={faRightFromBracket}
-                  className='text-figmaBlue'
-                />
-              </p>
-              <p className='text-figmaBlack'>Log out</p>
-            </div>
-          </a>
-        </div>
+          <p className='flex items-center gap-2'>
+            <span className='bg-white rounded-full h-9 w-9 flex items-center justify-center'>
+              <FontAwesomeIcon icon={faUser} className='text-figmaBlue' />
+            </span>
+            <span className='text-figmaBlack'>My profile</span>
+          </p>
+        </a>
+
+        <a
+          href='/login'
+          className='p-[0.4rem] pr-[4rem] rounded-lg hover:bg-figmaGrayShade transition-all duration-200 inline-block'
+          onClick={logOutFront}
+        >
+          <p className='flex items-center gap-2'>
+            <span className='bg-white rounded-full h-9 w-9 flex items-center justify-center'>
+              <FontAwesomeIcon
+                icon={faRightFromBracket}
+                className='text-figmaBlue'
+              />
+            </span>
+            <span className='text-figmaBlack'>Log out</span>
+          </p>
+        </a>
       </div>
     </div>
   );
